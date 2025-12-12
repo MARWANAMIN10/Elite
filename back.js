55ec7cdeb8d26b051cb7c038f43ddfeb50050409
MARWANAMIN10 <marwanamin6112006@gmail.com>
2025-12-12 21:03:47 +0200

    Delete back.js

--- back.js
+++
@@ -1,8 +0,0 @@
-fetch("https://jsonplaceholder.typicode.com/users")
-.then((res)=>res.json())
-.then((data)=>console.log(data));
-
-const data={
-    username : "this is title",
-    body:"this is post body",
-}
